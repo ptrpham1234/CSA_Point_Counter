093d4cff12ab56650c680d5185503e486bdc0afb
Peter Pham <ptrpham2468@gmail.com>
2021-09-29 22:30:51 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+/*****************************************************************************************************************
+ * Project:             Program 1
+ * Author:              Peter Pham
+ * Date Started:        09/28/2021
+ * Compiler:            java
+ * IDE:                 IntelliJ
+ *
+ * Description:
+ *
+ *****************************************************************************************************************/
+/*****************************************************************************************************************
+ * File:                main.c
+ * Author:              Peter Pham (pxp180041)
+ * Date Started:        09/06/2021
+ *
+ * Description:
+ * Creates a parent server and a child process and allow them to communicate with each other with pipes. The child
+ * will call on the server to verify server information and the it will send an array of integers and the server
+ * will call on its functions to verify if it is in fact a magic square.
+ *
+ * Changelog:
+ *
+ *
+ * TODO: found out the requirements so you can hard code the magic square
+ *****************************************************************************************************************/
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

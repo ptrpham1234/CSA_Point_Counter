bad076ad184998eb7c421e6ff667cbb7ef61a7a2
Peter Pham <ptrpham2468@gmail.com>
2021-10-04 21:01:15 -0500

    grabbed the data from the facebook

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,20 @@
 /*****************************************************************************************************************
- * Project:             Program 1
+ * Project:             CSA_Point_Counter
  * Author:              Peter Pham
  * Date Started:        09/28/2021
  * Compiler:            java
  * IDE:                 IntelliJ
  *
  * Description:
- *
+ * The aim of this program is to grab member's posts using Facebook API and count the number of people tagged in the
+ * post and give the appropriate amount of points.
  *****************************************************************************************************************/
 /*****************************************************************************************************************
- * File:                main.c
+ * File:                Main.java
  * Author:              Peter Pham (pxp180041)
  * Date Started:        09/06/2021
  *
  * Description:
- * Creates a parent server and a child process and allow them to communicate with each other with pipes. The child
- * will call on the server to verify server information and the it will send an array of integers and the server
- * will call on its functions to verify if it is in fact a magic square.
  *
  * Changelog:
  *
@@ -25,9 +23,23 @@
  *****************************************************************************************************************/
 package com.company;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        
+        String token = "EAAR621eun4IBALJqUrPXbEICcyckxKwN809dJpN5YlP1cN2lNxedDYbYNANpOW9c4IkEVcwB45cSNJbPTREGLQRZAm6FlgE1FykZCVIz6zdWyaqIrlZANoN9Op59M6OxdCjqEuXnrPW02D2Xi7YWjrye8k97GbSV5YnWaC7bZAXTQuSBs88zbZCoAicGSKRONZBEkJ9gsaZCwnv7JvZCXaaWjJQghBgB5YMZD";
+
+        URL url = new URL("https://graph.facebook.com/v12.0/1095482641223244?fields=feed&access_token=" + token);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                System.out.println(line + "\n");
+            }
+        }
     }
 }
